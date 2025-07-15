72fd1d538a0e3f27b2b206be851d8d5353596def
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-16 02:26:28 +0300

    Example usage of another usage of conditional rendering; render if true, don't render if false

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,11 @@ import "./App.css";
 import MovieCard from "./component/MovieCard";
 
 function App() {
-  const movieNumber = 2;
+  const movieNumber = 1;
   return (
     <>
-      {movieNumber === 1 ? (
+      {movieNumber === 1 && (
         <MovieCard movie={{ title: "La Vita Bella", release_date: "1986" }} />
-      ) : (
-        <>
-          <MovieCard movie={{ title: "La Vita Bella", release_date: "1986" }} />
-          <MovieCard movie={{ title: "Star Wars", release_date: "1992" }} />
-        </>
       )}
     </>
   );
