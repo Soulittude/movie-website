afafe3e7f6507b740ccadd70b0b1a21cca755b56
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-16 03:21:19 +0300

    React Router Dom Usage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
+import Favorites from "./pages/Favorites";
 
 function App() {
   const movieNumber = 1;
   return (
     <>
-      <Home />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   const [searchQuery, setSearchQuery] = useState("");
   const movies = [
     { id: 1, title: "John Wick", release_date: "2011" },
-    { id: 2, title: "Matrix", release_date: "2000" },
+    { id: 2, title: "The Matrix", release_date: "2000" },
     { id: 3, title: "Terminator", release_date: "1998" },
     { id: 4, title: "The Godfather", release_date: "1956" },
   ];
@@ -31,12 +31,9 @@ function Home() {
         </button>
       </form>
       <div className="movies-grid">
-        {movies.map(
-          (movie) =>
-            movie.title.toLowerCase().startsWith(searchQuery) && (
-              <MovieCard movie={movie} key={movie.id} />
-            )
-        )}
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
       </div>
     </div>
   );
