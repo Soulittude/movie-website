a45854b5e36684d0313b2bd0dbc9c20d0d19573d
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-17 02:40:27 +0300

    API setup

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "fb9e23587fa41c1c27d0a86879b72b98";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
