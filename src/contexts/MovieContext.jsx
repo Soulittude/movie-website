c899ee30e72eb9cbb2279353295b790ca03dee85
soulittude <79145776+Soulittude@users.noreply.github.com>
2025-07-17 03:52:50 +0300

    Bug fixed: now it still keeps locatstorage even refresh the page.

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -5,13 +5,9 @@ const MovieContext = createContext();
 export const useMovieContext = () => useContext(MovieContext);
 
 export const MovieProvider = ({ children }) => {
-  const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    const storedFavs = localStorage.getItem("favorites");
-
-    if (storedFavs) setFavorites(JSON.parse(storedFavs));
-  }, []);
+  const [favorites, setFavorites] = useState(() => {
+    return JSON.parse(localStorage.getItem("favorites")) || [];
+  });
 
   useEffect(() => {
     localStorage.setItem("favorites", JSON.stringify(favorites));
